64f397f4c121d031ddecac7691a152093dcbbf65
3DTHECAPO <andrewhampton113@gmail.com>
2026-03-26 17:52:19 -0700

    Delete nfc.js

--- nfc.js
+++
@@ -1,7 +0,0 @@
-const p=new URLSearchParams(window.location.search);
-const u=p.get('unlock');
-const show=id=>document.getElementById(id).classList.remove('hidden');
-if(u==='album')show('album');
-if(u==='track')show('track');
-if(u==='video')show('video');
-document.getElementById('status').innerText=u?u+' unlocked':'no unlock';
