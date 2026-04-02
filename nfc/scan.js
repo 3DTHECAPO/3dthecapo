64ddf36c6f5da3aeab3bce7b4ed55260829fc9e7
3DTHECAPO <andrewhampton113@gmail.com>
2026-04-01 18:07:58 -0700

    Add files via upload

--- nfc/scan.js
+++ nfc/scan.js
@@ -1,5 +1,5 @@
 function enterVault(){
-  const code = document.getElementById('code').value.toUpperCase();
-  if(!code) return alert('Enter code');
-  window.location.href = './index.html?nfc=1&code=' + code;
+  const code=document.getElementById('code').value.toUpperCase();
+  if(!code){alert('Enter code');return;}
+  window.location.href='./index.html?nfc=1&code='+code;
 }
