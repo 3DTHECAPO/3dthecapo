3745aa77b769a645c5a109de00766e9a7f542ad0
3DTHECAPO <andrewhampton113@gmail.com>
2026-04-04 13:21:21 -0700

    Delete nfc/nfc/game-vault directory

--- nfc/nfc/game-vault/shared.js
+++
@@ -1,9 +0,0 @@
-
-window.GV = {
-  params: new URLSearchParams(location.search),
-  isBonus() { return this.params.get('nfc') === '1' || localStorage.getItem('gv_bonus') === '1'; },
-  setReward(key, value='1'){ localStorage.setItem('gv_reward_'+key, value); },
-  getReward(key){ return localStorage.getItem('gv_reward_'+key); },
-  enableBonus(){ localStorage.setItem('gv_bonus','1'); }
-};
-if (GV.params.get('nfc') === '1') GV.enableBonus();
