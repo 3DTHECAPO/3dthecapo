19b7099890bddb4216a835fb6ebd9f279171d54c
3DTHECAPO <andrewhampton113@gmail.com>
2026-05-07 18:35:04 -0700

    Update multiplayer-config.js

--- nfc/shared/multiplayer-config.js
+++ nfc/shared/multiplayer-config.js
@@ -4,8 +4,8 @@
 */
 window.PLAY3D_MULTIPLAYER_CONFIG = {
   provider: "supabase",
-  supabaseUrl: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  supabaseAnonKey: "PASTE_SUPABASE_ANON_KEY_HERE",
+  supabaseUrl: "https://fupoedrovfloudefyzna.supabase.co/rest/v1/",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1cG9lZHJvdmZsb3VkZWZ5em5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NzU2NjEsImV4cCI6MjA5MjM1MTY2MX0.CGgOxXXSXWMjNPcnQR_zMBHk8WkWSb0lhcNlTfCR4xo",
   roomTable: "play3d_rooms",
   moveTable: "play3d_moves"
 };
